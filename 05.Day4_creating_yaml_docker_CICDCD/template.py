f64a076f14641f049ed62818dac476f892aabeff
rashmiKumari03 <rashmi15497@gmail.com>
2023-11-08 12:14:05 +0530

    update in termplate utils.py

--- 05.Day4_creating_yaml_docker_CICDCD/template.py
+++ 05.Day4_creating_yaml_docker_CICDCD/template.py
@@ -16,6 +16,7 @@ list_of_files=[
     f"src/{package_name}/logger.py",
     f"src/{package_name}/exception.py",
     f"src/{package_name}/utils/__init__.py",
+    f"src/{package_name}/utils/utils.py",
     "notebooks/research.ipynb",
     "notebooks/data/.gitkeep",
     "requirements.txt",
